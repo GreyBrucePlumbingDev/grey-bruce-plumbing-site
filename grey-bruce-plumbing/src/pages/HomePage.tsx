0257df3552f2d80f879dd8d0790ae22827b9e383
MARK-JPC <148413337+MARK-JPC@users.noreply.github.com>
2025-03-11 16:03:32 -0600

    removed duplicate navbar

--- grey-bruce-plumbing/src/pages/HomePage.tsx
+++ grey-bruce-plumbing/src/pages/HomePage.tsx
@@ -11,7 +11,6 @@ const HomePage = () => {
 
   return (
     <div>
-      <Navbar />
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content text-center">
           <div className="max-w-md">
